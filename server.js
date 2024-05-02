b4a784af986f5fdfd0c857f364b00d403a1699b3
4Cross <105809123+4CROS2@users.noreply.github.com>
2024-05-01 19:12:35 -0500

    channel fished

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const xd = require('./index')
 
 async function getData() {
-    xd.channels({country:'usa',page:1}).then((data)=>console.log(data))
+    /* xd.channelDetails({
+        path:'/sexmex_xxx',
+        page:1
+    }).then((data)=>console.log(data)) */
+    await xd.videos.newFresh({page:2}).then((video)=>console.log(video))
 }
 
 getData()
